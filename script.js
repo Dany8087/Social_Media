367373c522b3a7c5c371b78c1b2beb04d74edb95
Dany8087 <dnyanesh.s@prometteur.in>
2022-06-14 15:38:06 +0530

    insta-clone

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+
 var modal = document.getElementById("myModal");
 var modalR = document.getElementById("myModalR");
-var modalU = document.getElementById("myModalU")
+var modalU = document.getElementById("myModalU");
 
 
 var btn = document.getElementById("mylogin");
 var btnR = document.getElementById("myRegister");
-var btnU = document.getElementById("Upload")
+var btnU = document.getElementById("Upload");
 
 
 var span = document.getElementsByClassName("close")[0];
 var spanR = document.getElementsByClassName("closeR")[0];
 var spanU = document.getElementsByClassName("closeU")[0];
 
+// const upload = document.getElementById('Upload');
+// upload.addEventListener('click', uploadPic);
+
+// uploadPic(){
+
+// }
 
 btn.onclick = function () {
   modal.style.display = "block";
 }
-
 btnR.onclick = function () {
   modalR.style.display = "block";
 }
@@ -42,13 +48,11 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 }
-
 window.onclick = function (event) {
   if (event.target == modalR) {
     modalR.style.display = "none";
   }
 }
-
 window.onclick = function (event) {
   if (event.target == modalU) {
     modalU.style.display = "none";
@@ -63,4 +67,51 @@ window.onclick = function (event) {
 
 // }
 
+function signup(e){
 
+  const email = document.getElementById('email1').value;
+  const username = document.getElementById('username1').value;
+  const pass = document.getElementById('password1').value;
+
+  const user = {
+    email: email,
+    username:username,
+    password: pass,
+  };
+
+  const Json =JSON.stringify(user);
+  localStorage.setItem(username, Json);
+  console.log('user added');
+
+}
+
+function loginFunc(e){
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const pass = document.getElementById('password').value;
+  const feed = document.getElementById('feed');
+
+  const user = localStorage.getItem(username);
+  const data = JSON.parse(user);
+  console.log(data);
+
+  if(user == null){
+    feed.innerHTML = 'wrong username';
+  }else if(username == data.username && pass == data.password){
+    feed.innerHTML += `
+    <div class="card-columns">
+    <div class="card">
+      <img src="https://cdn.motor1.com/images/mgl/wBNko/s1/no.-48-bugatti-veyron-super-sport-for-sale.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+      </div>
+    </div>
+  </div>
+    `;
+    uploadPic();
+  }else{
+    feed.innerHTML ='wrong password';
+  }
+
+}
