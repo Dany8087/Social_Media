62063d7cd692ceda921fd4fc90664a2c63e9dd57
Dany8087 <dnyanesh.s@prometteur.in>
2022-06-15 14:46:23 +0530

    login done

--- script.js
+++ script.js
@@ -1,118 +1,118 @@
 
-var modal = document.getElementById("myModal");
-var modalR = document.getElementById("myModalR");
-var modalU = document.getElementById("myModalU");
+// var modal = document.getElementById("myModal");
+// var modalR = document.getElementById("myModalR");
+// var modalU = document.getElementById("myModalU");
 
 
-var btn = document.getElementById("mylogin");
-var btnR = document.getElementById("myRegister");
-var btnU = document.getElementById("Upload");
+// var btn = document.getElementById("mylogin");
+// var btnR = document.getElementById("myRegister");
+// var btnU = document.getElementById("Upload");
 
 
-var span = document.getElementsByClassName("close")[0];
-var spanR = document.getElementsByClassName("closeR")[0];
-var spanU = document.getElementsByClassName("closeU")[0];
+// var span = document.getElementsByClassName("close")[0];
+// var spanR = document.getElementsByClassName("closeR")[0];
+// var spanU = document.getElementsByClassName("closeU")[0];
 
-// const upload = document.getElementById('Upload');
-// upload.addEventListener('click', uploadPic);
+// // const upload = document.getElementById('Upload');
+// // upload.addEventListener('click', uploadPic);
 
-// uploadPic(){
+// // uploadPic(){
 
+// // }
+
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// }
+// btnR.onclick = function () {
+//   modalR.style.display = "block";
+// }
+// btnU.onclick = function () {
+//   modalU.style.display = "block";
 // }
 
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-btnR.onclick = function () {
-  modalR.style.display = "block";
-}
-btnU.onclick = function () {
-  modalU.style.display = "block";
-}
 
 
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
-spanR.onclick = function () {
-  modalR.style.display = "none";
-}
-spanU.onclick = function () {
-  modalU.style.display = "none";
-}
-
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-window.onclick = function (event) {
-  if (event.target == modalR) {
-    modalR.style.display = "none";
-  }
-}
-window.onclick = function (event) {
-  if (event.target == modalU) {
-    modalU.style.display = "none";
-  }
-}
-
-// const upload = document.getElementById("Upload")
-
-// upload.addEventListener("click", uploadData);
-
-// function uploadData() {
-
+// span.onclick = function () {
+//   modal.style.display = "none";
+// }
+// spanR.onclick = function () {
+//   modalR.style.display = "none";
+// }
+// spanU.onclick = function () {
+//   modalU.style.display = "none";
 // }
 
-// function signup(e){
 
-//   const email = document.getElementById('email1').value;
-//   const username = document.getElementById('username1').value;
-//   const pass = document.getElementById('password1').value;
-
-//   const user = {
-//     email: email,
-//     username:username,
-//     password: pass,
-//   };
-
-//   const Json =JSON.stringify(user);
-//   localStorage.setItem(username, Json);
-//   console.log('user added');
-
-// }
-
-// function loginFunc(e){
-//   event.preventDefault();
-
-//   const username = document.getElementById('username').value;
-//   const pass = document.getElementById('password').value;
-//   const feed = document.getElementById('feed');
-
-//   const user = localStorage.getItem(username);
-//   const data = JSON.parse(user);
-//   console.log(data);
-
-//   if(user == null){
-//     feed.innerHTML = 'wrong username';
-//   }else if(username == data.username && pass == data.password){
-//     feed.innerHTML += `
-//     <div class="card-columns">
-//     <div class="card">
-//       <img src="https://cdn.motor1.com/images/mgl/wBNko/s1/no.-48-bugatti-veyron-super-sport-for-sale.jpg" class="card-img-top" alt="...">
-//       <div class="card-body">
-//           <h5 class="card-title">Card title</h5>
-//       </div>
-//     </div>
-//   </div>
-//     `;
-//     uploadPic();
-//   }else{
-//     feed.innerHTML ='wrong password';
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
 //   }
 // }
+// window.onclick = function (event) {
+//   if (event.target == modalR) {
+//     modalR.style.display = "none";
+//   }
+// }
+// window.onclick = function (event) {
+//   if (event.target == modalU) {
+//     modalU.style.display = "none";
+//   }
+// }
+
+// // const upload = document.getElementById("Upload")
+
+// // upload.addEventListener("click", uploadData);
+
+// // function uploadData() {
+
+// // }
+
+// // function signup(e){
+
+// //   const email = document.getElementById('email1').value;
+// //   const username = document.getElementById('username1').value;
+// //   const pass = document.getElementById('password1').value;
+
+// //   const user = {
+// //     email: email,
+// //     username:username,
+// //     password: pass,
+// //   };
+
+// //   const Json =JSON.stringify(user);
+// //   localStorage.setItem(username, Json);
+// //   console.log('user added');
+
+// // }
+
+// // function loginFunc(e){
+// //   event.preventDefault();
+
+// //   const username = document.getElementById('username').value;
+// //   const pass = document.getElementById('password').value;
+// //   const feed = document.getElementById('feed');
+
+// //   const user = localStorage.getItem(username);
+// //   const data = JSON.parse(user);
+// //   console.log(data);
+
+// //   if(user == null){
+// //     feed.innerHTML = 'wrong username';
+// //   }else if(username == data.username && pass == data.password){
+// //     feed.innerHTML += `
+// //     <div class="card-columns">
+// //     <div class="card">
+// //       <img src="https://cdn.motor1.com/images/mgl/wBNko/s1/no.-48-bugatti-veyron-super-sport-for-sale.jpg" class="card-img-top" alt="...">
+// //       <div class="card-body">
+// //           <h5 class="card-title">Card title</h5>
+// //       </div>
+// //     </div>
+// //   </div>
+// //     `;
+// //     uploadPic();
+// //   }else{
+// //     feed.innerHTML ='wrong password';
+// //   }
+// // }
 
 
