d71bc100366eff8faf6b553ff1842f001358cbc8
Dany8087 <dnyanesh.s@prometteur.in>
2022-06-22 10:57:13 +0530

    profile picture update done

--- script/auth.js
+++
@@ -1,7 +0,0 @@
-
-function signup(e){
-    e.preventDefault()
-    const email = document.querySelector('#exampleInputEmail1')
-    const password = document.querySelector('#exampleInputPassword1')
-    console.log(email,password)
-}
